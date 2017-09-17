f30c28b1307af6ea6e8e618e335cc4927f4cab0b
Jason Xiong <JasonJianXiong@gmail.com>
2017-09-17 17:11:03 -0400

    sep 17 2017 version2

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('myFirstApp', []);
+
+    .controller('MyFirstController', function ($scope) {
+        $scope.name = "jason";
+        $scope.sayHello = function () {
+            return "hello, jason.";
+        };
+
+    });
+
+})(); // ()();IIFE (search what it is)
